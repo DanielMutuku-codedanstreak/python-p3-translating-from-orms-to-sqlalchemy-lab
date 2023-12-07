0f83092ed7ed5bb58cef05d33124497355cf9baf
Daniel Mutuku <danielmakau507@gmail.com>
2023-12-07 14:41:11 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,31 @@
 from models import Dog
 
-def create_table(base):
-    pass
+def create_table(base, engine):
+    # Create the table for the Dog model in the specified base.
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    # Add the dog instance to the session and commit the changes.
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    # Return a list of all Dog instances from the database.
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    # Return the Dog instance with the given name.
+    return session.query(Dog).filter_by(name=name).first()
 
 def find_by_id(session, id):
-    pass
+    # Return the Dog instance with the given id.
+    return session.query(Dog).get(id)
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    # Return the Dog instance with the given name and breed.
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    # Update the breed of the specified Dog instance and commit the changes.
+    dog.breed = breed
+    session.commit()
